6809ff6c6416340fa94c1a23d3b7329faea8a952
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2024-05-03 08:50:54 +0200

    test(session): add a test for the response cookie

--- backend/utils/session/session_test.go
+++ backend/utils/session/session_test.go
@@ -29,7 +29,7 @@ func TestSessionSave(t *testing.T) {
 		for k := range sessions {
 			keys = append(keys, k)
 		}
-		
+
 		s := sessions[keys[0]]
 		assert.Equalf(t, "helloworld", s.ip, "expected ip to be helloworld, got %s", s.ip)
 	})
@@ -41,8 +41,8 @@ func TestSessionSave(t *testing.T) {
 		r := httptest.NewRequest(http.MethodGet, "/", nil)
 
 		r.AddCookie(&http.Cookie{
-			Name: sessionCookieName,
-			Value: "random",
+			Name:   sessionCookieName,
+			Value:  "random",
 			MaxAge: 60,
 		})
 
@@ -56,7 +56,7 @@ func TestSessionSave(t *testing.T) {
 		for k := range sessions {
 			keys = append(keys, k)
 		}
-		
+
 		s := sessions[keys[0]]
 		assert.Equalf(t, "helloworld", s.ip, "expected ip to be helloworld, got %s", s.ip)
 	})
@@ -69,7 +69,7 @@ func TestSessionSave(t *testing.T) {
 
 		s := &session{
 			values: make(map[string]any),
-			ip: ip,
+			ip:     ip,
 		}
 		s.Set("jon", "snow")
 		sessions[sessionState] = s
@@ -79,8 +79,8 @@ func TestSessionSave(t *testing.T) {
 		r.RemoteAddr = ip
 
 		r.AddCookie(&http.Cookie{
-			Name: sessionCookieName,
-			Value: sessionState,
+			Name:   sessionCookieName,
+			Value:  sessionState,
 			MaxAge: 60,
 		})
 
@@ -97,7 +97,7 @@ func TestSessionSave(t *testing.T) {
 
 		s := &session{
 			values: make(map[string]any),
-			ip: ip,
+			ip:     ip,
 		}
 		s.Set("jon", "snow")
 		sessions[sessionState] = s
@@ -107,8 +107,8 @@ func TestSessionSave(t *testing.T) {
 		r.RemoteAddr = "winterfell"
 
 		r.AddCookie(&http.Cookie{
-			Name: sessionCookieName,
-			Value: sessionState,
+			Name:   sessionCookieName,
+			Value:  sessionState,
 			MaxAge: 60,
 		})
 
@@ -116,5 +116,30 @@ func TestSessionSave(t *testing.T) {
 
 		rs := sessions[sessionState]
 		assert.Nilf(t, rs, "expected session to be cleared, but got %v", rs)
+	})
+}
+
+func TestSessionSaveSetCookie(t *testing.T) {
+	t.Run("should set a cookie with the session state as value", func(t *testing.T) {
+		clean()
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		r.RemoteAddr = "192.1.1.1"
+
+		assert.Equal(t, 0, len(sessions))
+		Save(w, r)
+		assert.Equal(t, 1, len(sessions))
+
+		cookie := w.Result().Cookies()[0]
+		assert.Equalf(t, sessionCookieName, cookie.Name, "expected cookie name to be %s, got %s", sessionCookieName, cookie.Name)
+		assert.Equalf(t, sessions[cookie.Value].ip, r.RemoteAddr, "expected ip to be %s, got %s", r.RemoteAddr, sessions[cookie.Value].ip)
+		assert.NotEmpty(t, cookie.Value, "expected cookie value to be set")
+		assert.True(t, cookie.HttpOnly, "expected cookie to be http only")
+		assert.True(t, cookie.Secure, "expected cookie to be secure")
+		assert.Equal(t, "/", cookie.Path, "expected cookie path to be /")
+		assert.Equal(t, 60*60*24*7, cookie.MaxAge, "expected cookie max age to be 604800")
+		assert.Equal(t, http.SameSiteLaxMode, cookie.SameSite, "expected cookie same site to be lax")
 	})
 }
